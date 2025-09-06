25b9172e15a70ce967da51749b7af3cae2fec8e6
Misha_Ivchenko <ivchenkomykhailo@gmail.com>
2025-09-06 21:18:02 +0300

    Create the service for id generation

---
+++ src/services/idGeneratorService.ts
@@ -0,0 +1,21 @@
+class IdGenerator {
+  private currentId: number;
+
+  constructor(start: number = 0) {
+    this.currentId = start;
+  }
+
+  public next(): number {
+    return this.currentId++;
+  }
+
+  public reset(start: number = 1): void {
+    this.currentId = start;
+  }
+
+  public peek(): number {
+    return this.currentId;
+  }
+}
+
+export const idGeneratorService = new IdGenerator(0);

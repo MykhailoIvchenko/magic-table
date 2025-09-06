f3b1d6a67cc3868c5ed86f950c24f2d2d47ffcfb
Misha_Ivchenko <ivchenkomykhailo@gmail.com>
2025-09-06 12:41:52 +0300

    Implement the basic button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
+import Button from './components/ui/button/Button';
 import Input from './components/ui/input/Input';
 
 function App() {
   return (
-    <>
-      <Input label={'Test label'} error={'Test error'}/>
-    </>
+    <div style={{ width: '200px', margin: '20px auto' }}>
+      <Input label={'Test label'} error={'Test error'} />
+
+      <Button>Test button</Button>
+    </div>
   );
 }
 
